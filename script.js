d11fe97e252b61c48f8cd57de015c5c768e89a22
yamilzavala <zavalayamil9@gmail.com>
2022-07-17 01:25:38 -0300

    javascript functionality

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 //Api Unsplash
 const apiKey = 'ohTyNO9Ul5jlwxaueNJJLnIG8YWJ4UDUUkZH1oWo3y0';
-const countResult = 10;
+let countResult = 5;
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${countResult}`;
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
 
 let photosArray = [];
 const loader = document.getElementById('loader')
@@ -14,7 +18,19 @@ function setAttributtes(element, attributes){
     }
 }
 
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+        countResult = 30;
+    }
+    console.log('loaded img')
+}
+
 function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     //iterate in the array and for each photo
     photosArray.forEach(photo => {
         //create link and set attributes
@@ -29,7 +45,8 @@ function displayPhotos() {
             src: photo.urls.regular,
             alt: photo.alt_description,
             title: photo.alt_description,
-        })
+        });
+        img.addEventListener('load',imageLoaded);
         //put img inside a, and then both inside the container
         item.appendChild(img)
         container.appendChild(item)
@@ -45,5 +62,15 @@ async function getRandomPhoto() {
         console.log(error)
     }
 }
+
+
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000
+       && ready) {
+        ready = false;
+        getRandomPhoto();
+    }
+})
+
 
 getRandomPhoto()
